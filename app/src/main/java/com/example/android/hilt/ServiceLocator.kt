2f033c8ccb52de4ff541cb0b70393b073380ba11
felixrp74 <felixrp74@gmail.com>
2022-08-04 22:09:01 -0500

    hilt modules

--- app/src/main/java/com/example/android/hilt/ServiceLocator.kt
+++ app/src/main/java/com/example/android/hilt/ServiceLocator.kt
@@ -25,6 +25,7 @@ import com.example.android.hilt.navigator.AppNavigator
 import com.example.android.hilt.navigator.AppNavigatorImpl
 import com.example.android.hilt.util.DateFormatter
 
+
 class ServiceLocator(applicationContext: Context) {
 
     private val logsDatabase = Room.databaseBuilder(
