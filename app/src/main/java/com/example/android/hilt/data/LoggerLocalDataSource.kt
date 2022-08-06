23d952cee726ac17c183ba7cdc5d7002a7991133
felixrp74 <felixrp74@gmail.com>
2022-08-05 21:55:45 -0500

    Providing interfaces with bind and Qualifiers

--- app/src/main/java/com/example/android/hilt/data/LoggerLocalDataSource.kt
+++ app/src/main/java/com/example/android/hilt/data/LoggerLocalDataSource.kt
@@ -27,13 +27,17 @@ import javax.inject.Singleton
  * Data manager class that handles data manipulation between the database and the UI.
  */
 @Singleton
-class LoggerLocalDataSource @Inject constructor(private val logDao: LogDao) {
+class LoggerLocalDataSource @Inject constructor(private val logDao: LogDao) : LoggerDataSource {
 
     private val executorService: ExecutorService = Executors.newFixedThreadPool(4)
     private val mainThreadHandler by lazy {
         Handler(Looper.getMainLooper())
     }
 
+    override fun addLog(msg: String) {  }
+    override fun getAllLogs(callback: (List<Log>) -> Unit) {  }
+    override fun removeLogs() {  }
+/*
     fun addLog(msg: String) {
         executorService.execute {
             logDao.insertAll(
@@ -57,4 +61,5 @@ class LoggerLocalDataSource @Inject constructor(private val logDao: LogDao) {
             logDao.nukeTable()
         }
     }
+    */
 }
